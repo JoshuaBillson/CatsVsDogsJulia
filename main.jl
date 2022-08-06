3b7d90c233923d85616dac2dc3ee1c7de9df030b
JoshuaBillson <jmbillson@outlook.com>
2022-08-06 12:27:52 -0600

    Implemented Metric Aggregators

--- main.jl
+++ main.jl
@@ -1,4 +1,4 @@
-using Plots, Images, FileIO, Statistics, Metalhead, Flux, MLUtils, Augmentor
+using Plots, Images, FileIO, Statistics, Metalhead, Flux, MLUtils, Augmentor, Metrics, ProgressBars
 using Pipe: @pipe
 
 const EPOCHS = 1
@@ -9,43 +9,40 @@ using .MLImagePipeline
 include("models.jl")
 using .CatsAndDogsModels
 
+include("utils.jl")
+using .Utils
+
 """Plot 16 randomly selected sample images from the provided image loader."""
-function plot_samples(v::LazyImageLoader)
-    sample_images = [@pipe sample.filename |> load(_) |> imresize(_, (256, 256)) for sample in rand(v.samples, 16)]
-    plot([plot(x, axis=nothing, margin=0Plots.mm) for x in sample_images]..., layout=(4, 4), size=(256*4, 256*4))
-    Plots.savefig("samples.png")
+function plot_samples(v::ClassificationPipeline, filename::String)
+    plot([MLImagePipeline.plot_sample(v, i) for i in rand(1:length(v), 16)]..., layout=(4, 4), size=(256*4, 256*4))
+    Plots.savefig(filename)
 end
 
 "Load the sample data into a LazyImageLoader"
 function load_data(data_directory::String)
     pipeline = (FlipX() * FlipY() * NoOp()) |> Rotate90(0.25)
     train, test = ClassificationPipeline(data_directory, LabelFromDirectoryName(["cat", "dog"]), pipeline, input_size=(256, 256))
+    plot_samples(train, "images/train.png")
+    plot_samples(test, "images/test.png")
     return DataLoader(train, batchsize=32, shuffle=true), DataLoader(test, batchsize=32, shuffle=false)
 end
 
-function accuracy(prediction, ground_truth, labels)
-    @pipe (Flux.onecold(prediction, labels) .== Flux.onecold(ground_truth, labels)) |> mean |> x -> x * 100
+function training_callback(prediction, label, aggregator::Utils.MetricAggregator, iteration, total_iterations, epoch)
+    evaluation = aggregator(prediction, label)
+    println("Epoch $epoch: $iteration / $total_iterations, $evaluation")
 end
 
-function evaluate(model, dataset)
-    predictions = [(model(x), y) for (x, y) in dataset]
-    acc = [accuracy(p, y, 0:1) for (p, y) in predictions] |> mean
-    loss = [Flux.crossentropy(p, y, dims=1) for (p, y) in predictions] |> mean
-    println("Validation Loss: $(round(loss, digits=5, base=10)), Validation Accuracy: $(round(acc, digits=2, base=10))%")
-end
-
-function training_callback(accs, losses, prediction, label, iteration, total_iterations, epoch)
-    accs = vcat(accs, accuracy(prediction, label, 0:1))
-    losses = vcat(losses, Flux.crossentropy(prediction, label, dims=1))
-    acc, l = mean(accs), mean(losses)
-    println("Epoch $epoch: $iteration / $total_iterations, Loss: $(round(l, digits=4, base=10)), Accuracy: $(round(acc, digits=2, base=10))%")
-    return accs, losses
+function evaluate_model(model, data, iter)
+    predictions = [(Array(model(x)), Array(y)) for (x, y) in data]
+    accuracy = [Metrics.categorical_accuracy(p, l) for (p, l) in predictions] |> mean
+    loss = [Flux.Losses.crossentropy(p, l) for (p, l) in predictions] |> mean
+    println(iter, "Test Loss: $(round(loss, digits=6, base=10)), Test Accuracy: $(round(accuracy * 100, digits=4, base=10))%")
 end
 
 "Main program loop"
 function main_function()
     # Create Model
-    model = pretrained_model() |> gpu
+    model = Flux.Chain(Metalhead.ResNet(34, pretrain=true), Flux.Dense(1000, 2), Flux.softmax) |> gpu
 
     # Get Parameters
     parameters = Flux.params(model[end-1])
@@ -59,18 +56,23 @@ function main_function()
     # Define Optimizaer
     opt = Flux.Optimise.ADAM(1e-4)
 
+    # Define Metrics
+    loss_accumulator = Utils.MetricAccumulator(Flux.Losses.crossentropy, m -> "Loss: $(round(m, digits=6, base=10))")
+    accuracy_accumulator = Utils.MetricAccumulator(Metrics.categorical_accuracy, m -> "Accuracy: $(round(m*100.0, digits=4, base=10))%")
+    aggregator =  Utils.MetricAggregator([loss_accumulator, accuracy_accumulator])
+
     # Training Loop
     for epoch in 1:EPOCHS
 
-        accs, losses = [], []
-        for (step, (x, y)) in enumerate(data_train)
+        iter = ProgressBar(data_train)
+        for (step, (x, y)) in enumerate(iter)
 
-            # Log Loss And Accuracy
-            accs, losses = training_callback(accs, losses, model(x), y, step, length(data_train), epoch)
+            # Log Loss And Accuracj
+            set_description(iter, aggregator(Array(model(x)), Array(y)))
 
-            # Evaluate On Test Set Every 100 Iterations
+            # Evaluate On Test Set Every 250 Iterations
             if step % 250 == 0
-                evaluate(model, data_test)
+                evaluate_model(model, data_test, iter)
             end
 
             # Compute Gradients
